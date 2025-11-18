1a47ac69774e836afed72486459294a0e935274c
Abhilash N <abhilashn08080@gmail.com>
2025-11-18 14:58:09 +0000

    i planned to add a spline animation to the hero image section section

---
+++ src/components/spline-animation.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Spline from '@splinetool/react-spline';
+
+export function SplineAnimation() {
+  return (
+    <div className="absolute inset-0">
+      <Spline scene="https://prod.spline.design/6VqApL23x-qsGf8g/scene.splinecode" />
+    </div>
+  );
+}
