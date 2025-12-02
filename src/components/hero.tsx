008d8e4fbb4c66c66c344dbc9f4de1ecee80af72
Abhilash N <abhilashn08080@gmail.com>
2025-12-02 14:39:32 +0000

    Error: Data read, but end of buffer not reached     at r0 (https://6000-

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import dynamic from 'next/dynamic';
 import { TerminalAnimation } from './terminal-animation';
-
-const SplineAnimation = dynamic(
-  () => import('./spline-animation').then((mod) => mod.SplineAnimation),
-  { ssr: false }
-);
 
 export function Hero() {
   return (
-    <section className="relative w-full h-[80vh] min-h-[600px] flex items-center justify-center text-center text-primary-foreground overflow-hidden">
-      <SplineAnimation />
+    <section className="relative w-full h-[80vh] min-h-[600px] flex items-center justify-center text-center text-primary-foreground overflow-hidden bg-background">
+      <div className="absolute inset-0 bg-black/30 backdrop-blur-sm"></div>
       <div className="relative z-10 container px-4 sm:px-6 lg:px-8 flex flex-col items-center">
         <TerminalAnimation />
       </div>
